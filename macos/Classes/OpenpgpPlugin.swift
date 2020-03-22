d4c78f2d6ae793777391cd2e4fbc53e1612b474d
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-03-21 22:23:09 -0500

    update plugin template for macos

--- macos/Classes/OpenpgpPlugin.swift
+++ macos/Classes/OpenpgpPlugin.swift
@@ -30,86 +30,51 @@ public class OpenPGPPlugin: NSObject, FlutterPlugin {
         let args = call.arguments as! [String: Any]
 
         switch call.method {
-        case "encryptPKCS1v15":
-            encryptPKCS1v15(
+        case "encrypt":
+            encrypt(
                     message: args["message"] as? String,
-                    pkcs12: args["pkcs12"] as? String,
+                    publicKey: args["publicKey"] as? String,
+                    result: result
+            )
+        case "decrypt":
+            decrypt(
+                    message: args["message"] as? String,
+                    privateKey: args["privateKey"] as? String,
                     passphrase: args["passphrase"] as? String,
                     result: result
             )
-        case "encryptOAEP":
-            encryptOAEP(
+        case "encryptSymmetric":
+            encryptSymmetric(
                     message: args["message"] as? String,
-                    label: args["label"] as? String,
-                    hashName: args["hashName"] as? String,
-                    pkcs12: args["pkcs12"] as? String,
+                    passphrase: args["passphrase"] as? String,
+                    options: args["options"] as! [String: Any],
+                    result: result
+            )
+        case "decryptSymmetric":
+            decryptSymmetric(
+                    message: args["message"] as? String,
+                    passphrase: args["passphrase"] as? String,
+                    options: args["options"] as! [String: Any],
+                    result: result
+            )
+        case "sign":
+            sign(
+                    message: args["message"] as? String,
+                    publicKey: args["publicKey"] as? String,
+                    privateKey: args["privateKey"] as? String,
                     passphrase: args["passphrase"] as? String,
                     result: result
             )
-        case "decryptOAEP":
-            decryptOAEP(
-                    message: args["message"] as? String,
-                    label: args["label"] as? String,
-                    hashName: args["hashName"] as? String,
-                    pkcs12: args["pkcs12"] as? String,
-                    passphrase: args["passphrase"] as? String,
-                    result: result
-            )
-        case "decryptPKCS1v15":
-            decryptPKCS1v15(
-                    message: args["message"] as? String,
-                    pkcs12: args["pkcs12"] as? String,
-                    passphrase: args["passphrase"] as? String,
-                    result: result
-            )
-        case "signPKCS1v15":
-            signPKCS1v15(
-                    message: args["message"] as? String,
-                    hashName: args["hashName"] as? String,
-                    pkcs12: args["pkcs12"] as? String,
-                    passphrase: args["passphrase"] as? String,
-                    result: result
-            )
-        case "signPSS":
-            signPSS(
-                    message: args["message"] as? String,
-                    hashName: args["hashName"] as? String,
-                    pkcs12: args["pkcs12"] as? String,
-                    passphrase: args["passphrase"] as? String,
-                    result: result
-            )
-        case "verifyPKCS1v15":
-            verifyPKCS1v15(
+        case "verify":
+            verify(
                     signature: args["signature"] as? String,
                     message: args["message"] as? String,
-                    hashName: args["hashName"] as? String,
-                    pkcs12: args["pkcs12"] as? String,
-                    passphrase: args["passphrase"] as? String,
-                    result: result
-            )
-        case "verifyPSS":
-            verifyPSS(
-                    signature: args["signature"] as? String,
-                    message: args["message"] as? String,
-                    hashName: args["hashName"] as? String,
-                    pkcs12: args["pkcs12"] as? String,
-                    passphrase: args["passphrase"] as? String,
-                    result: result
-            )
-        case "base64":
-            base64(
-                    message: args["message"] as? String,
-                    result: result
-            )
-        case "hash":
-            hash(
-                    message: args["message"] as? String,
-                    name: args["name"] as? String,
+                    publicKey: args["publicKey"] as? String,
                     result: result
             )
         case "generate":
             generate(
-                    bits: args["bits"] as? Int,
+                    options: args["options"] as! [String: Any],
                     result: result
             )
         default:
@@ -117,11 +82,11 @@ public class OpenPGPPlugin: NSObject, FlutterPlugin {
         }
     }
 
-    func encryptPKCS1v15(message: String?, pkcs12: String?, passphrase: String?, result: @escaping FlutterResult) {
+    func encrypt(message: String?, publicKey: String?, result: @escaping FlutterResult) {
         queue?.async(execute: {
             do {
                 var error: NSError?
-                let output = try self.instance?.encryptPKCS1v15(message, pkcs12: pkcs12, passphrase: passphrase, error: &error)
+                let output = try self.instance?.encrypt(message, publicKey: publicKey, error: &error)
 
                 if error != nil {
                     self.result(result, output: FlutterError(code: String(format: "%ld", error?.code ?? 0), message: error?.description, details: nil))
@@ -134,11 +99,11 @@ public class OpenPGPPlugin: NSObject, FlutterPlugin {
         })
     }
 
-    func encryptOAEP(message: String?, label: String?, hashName: String?, pkcs12: String?, passphrase: String?, result: @escaping FlutterResult) {
+    func decrypt(message: String?, privateKey: String?, passphrase: String?, result: @escaping FlutterResult) {
         queue?.async(execute: {
             do {
                 var error: NSError?
-                let output = try self.instance?.encryptOAEP(message, label: label, hashName: hashName, pkcs12: pkcs12, passphrase: passphrase, error: &error)
+                let output = try self.instance?.decrypt(message, privateKey: privateKey, passphrase: passphrase, error: &error)
 
                 if error != nil {
                     self.result(result, output: FlutterError(code: String(format: "%ld", error?.code ?? 0), message: error?.description, details: nil))
@@ -151,11 +116,11 @@ public class OpenPGPPlugin: NSObject, FlutterPlugin {
         })
     }
 
-    func decryptOAEP(message: String?, label: String?, hashName: String?, pkcs12: String?, passphrase: String?, result: @escaping FlutterResult) {
+    func encryptSymmetric(message: String?, passphrase: String?, options: [String: Any], result: @escaping FlutterResult) {
         queue?.async(execute: {
             do {
                 var error: NSError?
-                let output = try self.instance?.decryptOAEP(message, label: label, hashName: hashName, pkcs12: pkcs12, passphrase: passphrase, error: &error)
+                let output = try self.instance?.encryptSymmetric(message, passphrase: passphrase, options: getKeyOptions(options), error: &error)
 
                 if error != nil {
                     self.result(result, output: FlutterError(code: String(format: "%ld", error?.code ?? 0), message: error?.description, details: nil))
@@ -168,11 +133,11 @@ public class OpenPGPPlugin: NSObject, FlutterPlugin {
         })
     }
 
-    func decryptPKCS1v15(message: String?, pkcs12: String?, passphrase: String?, result: @escaping FlutterResult) {
+    func decryptSymmetric(message: String?, passphrase: passphrase?, options: [String: Any], result: @escaping FlutterResult) {
         queue?.async(execute: {
             do {
                 var error: NSError?
-                let output = try self.instance?.decryptPKCS1v15(message, pkcs12: pkcs12, passphrase: passphrase, error: &error)
+                let output = try self.instance?.decryptSymmetric(message, passphrase: passphrase, options: getKeyOptions(options), error: &error)
 
                 if error != nil {
                     self.result(result, output: FlutterError(code: String(format: "%ld", error?.code ?? 0), message: error?.description, details: nil))
@@ -185,11 +150,11 @@ public class OpenPGPPlugin: NSObject, FlutterPlugin {
         })
     }
 
-    func signPKCS1v15(message: String?, hashName: String?, pkcs12: String?, passphrase: String?, result: @escaping FlutterResult) {
+    func sign(message: String?, publicKey: String?, privateKey: String?, passphrase: String?, result: @escaping FlutterResult) {
         queue?.async(execute: {
             do {
                 var error: NSError?
-                let output = try self.instance?.signPKCS1v15(message, hashName: hashName, pkcs12: pkcs12, passphrase: passphrase, error: &error)
+                let output = try self.instance?.sign(message, publicKey: publicKey, privateKey: privateKey, passphrase: passphrase, error: &error)
 
                 if error != nil {
                     self.result(result, output: FlutterError(code: String(format: "%ld", error?.code ?? 0), message: error?.description, details: nil))
@@ -202,28 +167,11 @@ public class OpenPGPPlugin: NSObject, FlutterPlugin {
         })
     }
 
-    func signPSS(message: String?, hashName: String?, pkcs12: String?, passphrase: String?, result: @escaping FlutterResult) {
-        queue?.async(execute: {
-            do {
-                var error: NSError?
-                let output = try self.instance?.signPSS(message, hashName: hashName, pkcs12: pkcs12, passphrase: passphrase, error: &error)
-
-                if error != nil {
-                    self.result(result, output: FlutterError(code: String(format: "%ld", error?.code ?? 0), message: error?.description, details: nil))
-                } else {
-                    self.result(result, output: output)
-                }
-            } catch {
-                self.result(result, output: FlutterError(code: "error", message: error.localizedDescription, details: nil))
-            }
-        })
-    }
-
-    func verifyPKCS1v15(signature: String?, message: String?, hashName: String?, pkcs12: String?, passphrase: String?, result: @escaping FlutterResult) {
+    func verify(signature: String?, message: String?, publicKey: String?, result: @escaping FlutterResult) {
         queue?.async(execute: {
             do {
                 var ret0_: ObjCBool = false
-                _ = try self.instance?.verifyPKCS1v15(signature, message: message, hashName: hashName, pkcs12: pkcs12, passphrase: passphrase, ret0_: &ret0_)
+                _ = try self.instance?.verify(signature, message: message, publicKey: publicKey, ret0_: &ret0_)
 
                 self.result(result, output: ret0_.boolValue as NSNumber)
             } catch {
@@ -232,58 +180,10 @@ public class OpenPGPPlugin: NSObject, FlutterPlugin {
         })
     }
 
-    func verifyPSS(signature: String?, message: String?, hashName: String?, pkcs12: String?, passphrase: String?, result: @escaping FlutterResult) {
+    func generate(options: [String: Any], result: @escaping FlutterResult) {
         queue?.async(execute: {
             do {
-
-                var ret0_: ObjCBool = false
-                _ = try self.instance?.verifyPSS(signature, message: message, hashName: hashName, pkcs12: pkcs12, passphrase: passphrase, ret0_: &ret0_)
-
-                self.result(result, output: ret0_.boolValue as NSNumber)
-            } catch {
-                self.result(result, output: FlutterError(code: "error", message: error.localizedDescription, details: nil))
-            }
-        })
-    }
-
-    func hash(message: String?, name: String?, result: @escaping FlutterResult) {
-        queue?.async(execute: {
-            do {
-                var error: NSError?
-                let output = try self.instance?.hash(message, name: name, error: &error)
-
-                if error != nil {
-                    self.result(result, output: FlutterError(code: String(format: "%ld", error?.code ?? 0), message: error?.description, details: nil))
-                } else {
-                    self.result(result, output: output)
-                }
-            } catch {
-                self.result(result, output: FlutterError(code: "error", message: error.localizedDescription, details: nil))
-            }
-        })
-    }
-
-    func base64(message: String?, result: @escaping FlutterResult) {
-        queue?.async(execute: {
-            do {
-                var error: NSError?
-                let output = try self.instance?.base64(message, error: &error)
-
-                if error != nil {
-                    self.result(result, output: FlutterError(code: String(format: "%ld", error?.code ?? 0), message: error?.description, details: nil))
-                } else {
-                    self.result(result, output: output)
-                }
-            } catch {
-                self.result(result, output: FlutterError(code: "error", message: error.localizedDescription, details: nil))
-            }
-        })
-    }
-
-    func generate(bits: Int?, result: @escaping FlutterResult) {
-        queue?.async(execute: {
-            do {
-                let output = try self.instance?.generate(bits!)
+                let output = try self.instance?.generate(getOptions(options!))
 
                 self.result(result, output: [
                     "publicKey": output?.publicKey,
@@ -295,4 +195,52 @@ public class OpenPGPPlugin: NSObject, FlutterPlugin {
             }
         })
     }
+    
+    func getKeyOptions(_ map: [String : Any]?) -> OpenpgpKeyOptions? {
+        let options = OpenpgpKeyOptions()
+        if map == nil {
+            return options
+        }
+        if map?["cipher"] != nil {
+            options.cipher = map?["cipher"]
+        }
+        if map?["compression"] != nil {
+            options.compression = map?["compression"]
+        }
+        if map?["hash"] != nil {
+            options.hash = map?["hash"]
+        }
+        if map?["rsaBits"] != nil {
+            options.rsaBits = (map?["rsaBits"] as? NSNumber)?.floatValue ?? 0.0
+        }
+        if map?["compressionLevel"] != nil {
+            options.compressionLevel = (map?["compressionLevel"] as? NSNumber)?.floatValue ?? 0.0
+        }
+        return options
+    }
+    
+    func getOptions(_ map: [String : Any]?) -> OpenpgpOptions? {
+        let options = OpenpgpOptions()
+        if map == nil {
+            return options
+        }
+        if map?["name"] != nil {
+            options.name = map?["name"]
+        }
+        if map?["email"] != nil {
+            options.email = map?["email"]
+        }
+        if map?["comment"] != nil {
+            options.comment = map?["comment"]
+        }
+        if map?["passphrase"] != nil {
+            options.passphrase = map?["passphrase"]
+        }
+        if map?["keyOptions"] != nil {
+            options.keyOptions = getKeyOptions(map?["keyOptions"] as? [AnyHashable : Any])
+        }
+
+        return options
+    }
+
 }
