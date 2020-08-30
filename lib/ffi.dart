21cbb2b9519d511f5e8a15619a29f2db57063e3e
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-08-29 23:01:57 -0500

    first try with ffi

---
+++ lib/ffi.dart
@@ -0,0 +1,25 @@
+import "dart:ffi" as ffi;
+import 'dart:io' show Platform;
+import "package:path/path.dart" show join;
+import 'dart:io';
+
+ffi.DynamicLibrary _dlopenPlatformSpecific(String name, {String path}) {
+  String fullPath = _platformPath(name, path: path);
+  return ffi.DynamicLibrary.open(fullPath);
+}
+
+String _platformPath(String name, {String path = ''}) {
+  if (Platform.isMacOS) return path + "darwin" + name + ".dylib";
+  if (Platform.isLinux || Platform.isAndroid)
+    return path + name + "_linux_amd64.so";
+  if (Platform.isWindows) return path + name + ".dll";
+  throw Exception("Platform not implemented");
+}
+
+ffi.DynamicLibrary openLib() {
+  return _dlopenPlatformSpecific('openpgp',
+      path: join(
+        Directory(Platform.resolvedExecutable).parent.path,
+        'assets/binding/',
+      ));
+}
