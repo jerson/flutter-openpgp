f0e763aadbc04e46c57b34235e8e0ce5c5620ec0
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-09-21 23:35:44 -0500

    bridge macos no ffi

--- lib/openpgp.dart
+++ lib/openpgp.dart
@@ -8,10 +8,8 @@ import 'package:openpgp/bridge/model/bridge.pb.dart';
 
 class OpenPGP {
   static const MethodChannel _channel = const MethodChannel('openpgp');
-  static bool _bindingSupported = Platform.isMacOS ||
-      Platform.isWindows ||
-      Platform.isLinux ||
-      Platform.isFuchsia;
+  static bool _bindingSupported =
+      Platform.isWindows || Platform.isLinux || Platform.isFuchsia;
 
   static Future<Uint8List> _call(String message, Uint8List payload) async {
     if (_bindingSupported) {
@@ -62,141 +60,138 @@ class OpenPGP {
 
   static Future<String> decrypt(
       String message, String privateKey, String passphrase) async {
-      var request = DecryptRequest()
-        ..message = message
-        ..privateKey = privateKey
-        ..passphrase = passphrase;
-      return await _stringResponse("decrypt", request.writeToBuffer());
+    var request = DecryptRequest()
+      ..message = message
+      ..privateKey = privateKey
+      ..passphrase = passphrase;
+    return await _stringResponse("decrypt", request.writeToBuffer());
   }
 
   static Future<Uint8List> decryptBytes(
       Uint8List message, String privateKey, String passphrase) async {
-      var request = DecryptBytesRequest()
-        ..message = message
-        ..privateKey = privateKey
-        ..passphrase = passphrase;
-      return await _bytesResponse("decryptBytes", request.writeToBuffer());
+    var request = DecryptBytesRequest()
+      ..message = message
+      ..privateKey = privateKey
+      ..passphrase = passphrase;
+    return await _bytesResponse("decryptBytes", request.writeToBuffer());
   }
 
   static Future<String> encrypt(String message, String publicKey) async {
-      var request = EncryptRequest()
-        ..message = message
-        ..publicKey = publicKey;
-      return await _stringResponse("encrypt", request.writeToBuffer());
+    var request = EncryptRequest()
+      ..message = message
+      ..publicKey = publicKey;
+    return await _stringResponse("encrypt", request.writeToBuffer());
   }
 
   static Future<Uint8List> encryptBytes(
       Uint8List message, String publicKey) async {
-      var request = EncryptBytesRequest()
-        ..message = message
-        ..publicKey = publicKey;
-      return await _bytesResponse("encryptBytes", request.writeToBuffer());
+    var request = EncryptBytesRequest()
+      ..message = message
+      ..publicKey = publicKey;
+    return await _bytesResponse("encryptBytes", request.writeToBuffer());
   }
 
   static Future<String> sign(String message, String publicKey,
       String privateKey, String passphrase) async {
-      var request = SignRequest()
-        ..message = message
-        ..publicKey = publicKey
-        ..privateKey = privateKey
-        ..passphrase = passphrase;
-      return await _stringResponse("sign", request.writeToBuffer());
+    var request = SignRequest()
+      ..message = message
+      ..publicKey = publicKey
+      ..privateKey = privateKey
+      ..passphrase = passphrase;
+    return await _stringResponse("sign", request.writeToBuffer());
   }
 
   static Future<Uint8List> signBytes(Uint8List message, String publicKey,
       String privateKey, String passphrase) async {
-      var request = SignBytesRequest()
-        ..message = message
-        ..publicKey = publicKey
-        ..privateKey = privateKey
-        ..passphrase = passphrase;
-      return await _bytesResponse("signBytes", request.writeToBuffer());
+    var request = SignBytesRequest()
+      ..message = message
+      ..publicKey = publicKey
+      ..privateKey = privateKey
+      ..passphrase = passphrase;
+    return await _bytesResponse("signBytes", request.writeToBuffer());
   }
 
   static Future<String> signBytesToString(Uint8List message, String publicKey,
       String privateKey, String passphrase) async {
-      var request = SignBytesRequest()
-        ..message = message
-        ..publicKey = publicKey
-        ..privateKey = privateKey
-        ..passphrase = passphrase;
-      return await _stringResponse(
-          "signBytesToString", request.writeToBuffer());
+    var request = SignBytesRequest()
+      ..message = message
+      ..publicKey = publicKey
+      ..privateKey = privateKey
+      ..passphrase = passphrase;
+    return await _stringResponse("signBytesToString", request.writeToBuffer());
   }
 
   static Future<bool> verify(
       String signature, String message, String publicKey) async {
-      var request = VerifyRequest()
-        ..message = message
-        ..publicKey = publicKey
-        ..signature = signature;
-      return await _boolResponse("verify", request.writeToBuffer());
+    var request = VerifyRequest()
+      ..message = message
+      ..publicKey = publicKey
+      ..signature = signature;
+    return await _boolResponse("verify", request.writeToBuffer());
   }
 
   static Future<bool> verifyBytes(
       String signature, Uint8List message, String publicKey) async {
-      var request = VerifyBytesRequest()
-        ..message = message
-        ..publicKey = publicKey
-        ..signature = signature;
-      return await _boolResponse("verifyBytes", request.writeToBuffer());
+    var request = VerifyBytesRequest()
+      ..message = message
+      ..publicKey = publicKey
+      ..signature = signature;
+    return await _boolResponse("verifyBytes", request.writeToBuffer());
   }
 
   static Future<String> decryptSymmetric(String message, String passphrase,
       {KeyOptions options}) async {
-      var request = DecryptSymmetricRequest()
-        ..message = message
-        ..passphrase = passphrase;
-      if (options != null) {
-        request.options = options;
-      }
-      return await _stringResponse("decryptSymmetric", request.writeToBuffer());
+    var request = DecryptSymmetricRequest()
+      ..message = message
+      ..passphrase = passphrase;
+    if (options != null) {
+      request.options = options;
+    }
+    return await _stringResponse("decryptSymmetric", request.writeToBuffer());
   }
 
   static Future<Uint8List> decryptSymmetricBytes(
       Uint8List message, String passphrase,
       {KeyOptions options}) async {
-      var request = DecryptSymmetricBytesRequest()
-        ..message = message
-        ..passphrase = passphrase;
-      if (options != null) {
-        request.options = options;
-      }
-      return await _bytesResponse(
-          "decryptSymmetricBytes", request.writeToBuffer());
+    var request = DecryptSymmetricBytesRequest()
+      ..message = message
+      ..passphrase = passphrase;
+    if (options != null) {
+      request.options = options;
+    }
+    return await _bytesResponse(
+        "decryptSymmetricBytes", request.writeToBuffer());
   }
 
   static Future<String> encryptSymmetric(String message, String passphrase,
       {KeyOptions options}) async {
-      var request = EncryptSymmetricRequest()
-        ..message = message
-        ..passphrase = passphrase;
-      if (options != null) {
-        request.options = options;
-      }
-      return await _stringResponse("encryptSymmetric", request.writeToBuffer());
+    var request = EncryptSymmetricRequest()
+      ..message = message
+      ..passphrase = passphrase;
+    if (options != null) {
+      request.options = options;
+    }
+    return await _stringResponse("encryptSymmetric", request.writeToBuffer());
   }
 
   static Future<Uint8List> encryptSymmetricBytes(
       Uint8List message, String passphrase,
       {KeyOptions options}) async {
-      var request = EncryptSymmetricBytesRequest()
-        ..message = message
-        ..passphrase = passphrase;
-      if (options != null) {
-        request.options = options;
-      }
-      return await _bytesResponse(
-          "encryptSymmetricBytes", request.writeToBuffer());
+    var request = EncryptSymmetricBytesRequest()
+      ..message = message
+      ..passphrase = passphrase;
+    if (options != null) {
+      request.options = options;
+    }
+    return await _bytesResponse(
+        "encryptSymmetricBytes", request.writeToBuffer());
   }
 
   static Future<KeyPair> generate({Options options}) async {
-      var request = GenerateRequest();
-      if (options != null) {
-        request.options = options;
-      }
-      return await _keyPairResponse("generate", request.writeToBuffer());
-
+    var request = GenerateRequest();
+    if (options != null) {
+      request.options = options;
+    }
+    return await _keyPairResponse("generate", request.writeToBuffer());
   }
-
 }
