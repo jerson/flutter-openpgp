5b7f652751b79c58bdedb96e07b1e07550b93c07
Gerson Alexander Pardo Gamez <jerson@users.noreply.github.com>
2024-08-05 16:02:02 -0500

    chore: migrate to new wasm (#74)

--- lib/web/openpgp_web.dart
+++ lib/web/openpgp_web.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
-import 'dart:collection';
-import 'dart:html';
+import 'dart:js_interop';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:web/web.dart';
 
 class OpenpgpPlugin {
   var _counter = 0;
-  Worker worker = new Worker('assets/packages/openpgp/web/assets/worker.js');
+  Worker worker =
+      new Worker('assets/packages/openpgp/web/assets/worker.js'.toJS);
   Map<String, Completer<Uint8List>> completers = {};
 
   static void registerWith(Registrar registrar) {
@@ -26,27 +27,62 @@ class OpenpgpPlugin {
   }
 
   void listen() async {
-    worker.onMessage.listen((event) {
-      LinkedHashMap<dynamic, dynamic> data = event.data;
-      var completer = completers[data['id']];
+    void _onMessage(Event event) {
+      final msgEvent = event as MessageEvent;
+      final data = msgEvent.data as OpenpgpResponse;
+      var completer = completers[data.id];
       if (completer == null) {
         return;
       }
-      if (data['error'] != null && data['error'] != "") {
-        completer.completeError(data['error']);
+      if (data.error != null && data.error! != '') {
+        completer.completeError(data.error!);
       } else {
-        completer.complete(data['response']);
+        completer.complete(data.response?.toDart);
       }
-      completers.remove(data['id']);
-    });
+      completers.remove(data.id);
+    }
+
+    worker.onmessage = _onMessage.toJS;
   }
 
-  Future<Uint8List> bridgeCall(String name, Uint8List request) async {
+  Future<Uint8List> bridgeCall(String name, Uint8List? /*!*/ request) async {
     _counter++;
     var id = _counter.toString();
     var completer = new Completer<Uint8List>();
     completers[id] = completer;
-    worker.postMessage({'id': id, 'name': name, 'request': request});
+    worker.postMessage(OpenpgpRequest(
+      id: id,
+      name: name,
+      request: request?.toJS,
+    ));
     return completer.future;
   }
+}
+
+@JS()
+@anonymous
+extension type OpenpgpRequest._(JSObject _) implements JSObject {
+  external String id;
+  external String name;
+  external JSUint8Array? request;
+
+  external OpenpgpRequest({
+    String id,
+    String name,
+    JSUint8Array? request,
+  });
+}
+
+@JS()
+@anonymous
+extension type OpenpgpResponse._(JSObject _) implements JSObject {
+  external String id;
+  external String? error;
+  external JSUint8Array? response;
+
+  external OpenpgpResponse({
+    String id,
+    String? error,
+    JSUint8Array? response,
+  });
 }
